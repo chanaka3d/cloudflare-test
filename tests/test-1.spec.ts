ecdc7d4346b5fde8d722e106182d7c83cb3cb6ea
Chanaka Jayasena <chanaka3d@gmail.com>
2024-01-08 23:00:19 +0530

    x3

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,9 +1,9 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { test } from '@playwright/test';
-const { baseURL } = process.env;
+const BASE_URL = process.env.URL;
 
 test('poc', async ({ page }) => {
-  await page.goto(baseURL || 'http://localhost:3000');
+  await page.goto(BASE_URL || 'http://localhost:8000');
   await page.getByPlaceholder('Enter your name').click();
   await page.getByPlaceholder('Enter your name').fill('chanaka');
   await page.getByRole('button', { name: 'Set Name' }).click();
